ae145e254d13227d59da8f1b56b0c16052aee6fa
Sanjay Kumar <ersanjay426@gmail.com>
2026-06-24 01:42:25 +0530

    Detect cycle using slow & fast pointer

---
+++ src/collections/DetectCycle.java
@@ -0,0 +1,50 @@
+package collections;
+
+public class DetectCycle {
+
+    static class Node {
+
+        int value;
+        Node next;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public static boolean detectCycle(Node head) {
+
+        Node slow = head;
+        Node fast = head;
+
+        while (fast != null && fast.next != null) {
+
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast)
+                return true;
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+        Node n1 = new Node(1);
+        Node n2 = new Node(2);
+        Node n3 = new Node(3);
+        Node n4 = new Node(4);
+        Node n5 = new Node(5);
+        Node n6 = new Node(6);
+
+        n1.next = n2;
+        n2.next = n3;
+        n3.next = n4;
+        n4.next = n5;
+        n5.next = n6;
+        n6.next = n1;
+
+        System.out.println(detectCycle(n1));
+    }
+}
